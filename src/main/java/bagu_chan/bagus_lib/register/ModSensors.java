05285f7af7a9a266f730ac01993ab0f7a1ca4544
baguchan <shutux500@gmail.com>
2023-05-31 16:12:17 +0900

    1.19.4-1.2.0.1

--- src/main/java/bagu_chan/bagus_lib/register/ModSensors.java
+++ src/main/java/bagu_chan/bagus_lib/register/ModSensors.java
@@ -2,6 +2,7 @@ package bagu_chan.bagus_lib.register;
 
 import bagu_chan.bagus_lib.BagusLib;
 import bagu_chan.bagus_lib.entity.sensor.SmartNearestLivingEntitySensor;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.sensing.SensorType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -10,6 +11,6 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModSensors {
     public static final DeferredRegister<SensorType<?>> SENSOR_TYPES = DeferredRegister.create(ForgeRegistries.SENSOR_TYPES, BagusLib.MODID);
 
-    public static final RegistryObject<SensorType<SmartNearestLivingEntitySensor<?>>> SMART_NEAREST_LIVING_ENTITY_SENSOR = SENSOR_TYPES.register("smart_nearest_living_entity_sensor",
+    public static final RegistryObject<SensorType<SmartNearestLivingEntitySensor<LivingEntity>>> SMART_NEAREST_LIVING_ENTITY_SENSOR = SENSOR_TYPES.register("smart_nearest_living_entity_sensor",
             () -> new SensorType<>(SmartNearestLivingEntitySensor::new));
 }
