eee64c861b57a1b1f99959477a917308841c87db
baguchan <shutux500@gmail.com>
2023-07-21 17:54:50 +0900

    1.20.1-3.1.0 add LoopableAnimationTime and change AnimationTime

--- src/main/java/bagu_chan/bagus_lib/entity/LoopableAnimationTime.java
+++ src/main/java/bagu_chan/bagus_lib/entity/LoopableAnimationTime.java
@@ -3,12 +3,12 @@ package bagu_chan.bagus_lib.entity;
 import net.minecraft.world.entity.AnimationState;
 import net.minecraft.world.entity.Entity;
 
-public class AnimationTime {
+public class LoopableAnimationTime {
     private final AnimationState animationState = new AnimationState();
     private final int animationLength;
     private int animationTick;
 
-    public AnimationTime(int animationLength) {
+    public LoopableAnimationTime(int animationLength) {
         this.animationLength = animationLength;
     }
 
@@ -28,6 +28,7 @@ public class AnimationTime {
     public void tick(Entity entity) {
         if (this.animationTick < this.animationLength) {
             this.animationTick++;
+            this.animationState.startIfStopped(entity.tickCount);
         }
 
         if (this.animationTick >= this.animationLength) {
@@ -36,7 +37,6 @@ public class AnimationTime {
     }
 
     public void start(Entity entity) {
-        this.animationState.start(entity.tickCount);
         this.animationTick = 0;
     }
 }
