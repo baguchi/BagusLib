78f6a4ca0b2e5214aa8a281b012d1fb9e7d0e688
baguchan <shutux500@gmail.com>
2023-10-03 16:25:13 +0900

    add animation scale class 1.20.2-5.1.1

---
+++ src/main/java/bagu_chan/bagus_lib/entity/AnimationScale.java
@@ -0,0 +1,40 @@
+package bagu_chan.bagus_lib.entity;
+
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
+
+/*
+ * This class using For animation scaling(Such as when changing from walking to running animation)
+ */
+
+public class AnimationScale {
+
+    private boolean flag;
+    private float animationScale;
+    private float animationScale0;
+    private final float amount;
+
+
+    public AnimationScale(boolean flag) {
+        this(flag, 0.1F);
+    }
+
+    public AnimationScale(boolean flag, float amount) {
+        this.flag = flag;
+        this.amount = amount;
+    }
+
+
+    public void tick(Entity entity) {
+        this.animationScale0 = this.animationScale;
+        if (flag) {
+            this.animationScale = Mth.clamp(this.animationScale + amount, 0.0F, 1.0F);
+        } else {
+            this.animationScale = Mth.clamp(this.animationScale - amount, 0.0F, 1.0F);
+        }
+    }
+
+    public float getAnimationScale(float p_29570_) {
+        return Mth.lerp(p_29570_, this.animationScale0, this.animationScale);
+    }
+}
