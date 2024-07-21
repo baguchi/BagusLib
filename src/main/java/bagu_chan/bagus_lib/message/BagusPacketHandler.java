b7c9e33d6b34862f71f61e35af7ec9a3b0ba5943
baguchan <shutux500@gmail.com>
2024-07-21 19:10:40 +0900

    1.20.1-5.1.2 fixed packet

--- src/main/java/bagu_chan/bagus_lib/message/BagusPacketHandler.java
+++ src/main/java/bagu_chan/bagus_lib/message/BagusPacketHandler.java
@@ -1,39 +1,56 @@
 package bagu_chan.bagus_lib.message;
 
 import bagu_chan.bagus_lib.BagusLib;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
 
 public class BagusPacketHandler {
 
 
-    public static final String PROTOCOL_VERSION = "2";
-    public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
-            BagusLib.prefix("channel"),
-            () -> PROTOCOL_VERSION,
-            PROTOCOL_VERSION::equals,
-            PROTOCOL_VERSION::equals
-    );
+    public static final String NETWORK_PROTOCOL = "2";
+    public static final SimpleChannel CHANNEL = NetworkRegistry.ChannelBuilder.named(new ResourceLocation(BagusLib.MODID, "channel"))
+            .networkProtocolVersion(() -> NETWORK_PROTOCOL)
+            .clientAcceptedVersions(NETWORK_PROTOCOL::equals)
+            .serverAcceptedVersions(NETWORK_PROTOCOL::equals)
+            .simpleChannel();
 
     public static void setupMessages() {
-        CHANNEL.registerMessage(0, CameraMessage.class,
-                CameraMessage::writeToPacket, CameraMessage::readFromPacket, CameraMessage::handle);
-        CHANNEL.registerMessage(1, UpdateDataMessage.class,
-                UpdateDataMessage::writeToPacket, UpdateDataMessage::readFromPacket, UpdateDataMessage::handle);
-        CHANNEL.registerMessage(2, EntityCameraMessage.class,
-                EntityCameraMessage::writeToPacket, EntityCameraMessage::readFromPacket, EntityCameraMessage::handle);
-        CHANNEL.registerMessage(3, SyncEntityPacketToServer.class,
-                SyncEntityPacketToServer::writeToPacket, SyncEntityPacketToServer::readFromPacket, SyncEntityPacketToServer::handle);
-        CHANNEL.registerMessage(4, DialogMessage.class,
-                DialogMessage::writeToPacket, DialogMessage::readFromPacket, DialogMessage::handle);
-        CHANNEL.registerMessage(5, ImageDialogMessage.class,
-                ImageDialogMessage::writeToPacket, ImageDialogMessage::readFromPacket, ImageDialogMessage::handle);
-        CHANNEL.registerMessage(6, ItemStackDialogMessage.class,
-                ItemStackDialogMessage::writeToPacket, ItemStackDialogMessage::readFromPacket, ItemStackDialogMessage::handle);
-        CHANNEL.registerMessage(7, RemoveAllDialogMessage.class,
-                RemoveAllDialogMessage::writeToPacket, RemoveAllDialogMessage::readFromPacket, RemoveAllDialogMessage::handle);
-        CHANNEL.registerMessage(8, SyncBagusAnimationsMessage.class,
-                SyncBagusAnimationsMessage::writeToPacket, SyncBagusAnimationsMessage::readFromPacket, SyncBagusAnimationsMessage::handle);
-
+        CHANNEL.messageBuilder(CameraMessage.class, 0)
+                .encoder(CameraMessage::writeToPacket).decoder(CameraMessage::readFromPacket)
+                .consumerMainThread(CameraMessage::handle)
+                .add();
+        CHANNEL.messageBuilder(UpdateDataMessage.class, 1)
+                .encoder(UpdateDataMessage::writeToPacket).decoder(UpdateDataMessage::readFromPacket)
+                .consumerMainThread(UpdateDataMessage::handle)
+                .add();
+        CHANNEL.messageBuilder(EntityCameraMessage.class, 2)
+                .encoder(EntityCameraMessage::writeToPacket).decoder(EntityCameraMessage::readFromPacket)
+                .consumerMainThread(EntityCameraMessage::handle)
+                .add();
+        CHANNEL.messageBuilder(SyncEntityPacketToServer.class, 3)
+                .encoder(SyncEntityPacketToServer::writeToPacket).decoder(SyncEntityPacketToServer::readFromPacket)
+                .consumerMainThread(SyncEntityPacketToServer::handle)
+                .add();
+        CHANNEL.messageBuilder(DialogMessage.class, 4)
+                .encoder(DialogMessage::writeToPacket).decoder(DialogMessage::readFromPacket)
+                .consumerMainThread(DialogMessage::handle)
+                .add();
+        CHANNEL.messageBuilder(ImageDialogMessage.class, 5)
+                .encoder(ImageDialogMessage::writeToPacket).decoder(ImageDialogMessage::readFromPacket)
+                .consumerMainThread(ImageDialogMessage::handle)
+                .add();
+        CHANNEL.messageBuilder(ItemStackDialogMessage.class, 6)
+                .encoder(ItemStackDialogMessage::writeToPacket).decoder(ItemStackDialogMessage::readFromPacket)
+                .consumerMainThread(ItemStackDialogMessage::handle)
+                .add();
+        CHANNEL.messageBuilder(RemoveAllDialogMessage.class, 7)
+                .encoder(RemoveAllDialogMessage::writeToPacket).decoder(RemoveAllDialogMessage::readFromPacket)
+                .consumerMainThread(RemoveAllDialogMessage::handle)
+                .add();
+        CHANNEL.messageBuilder(SyncBagusAnimationsMessage.class, 8)
+                .encoder(SyncBagusAnimationsMessage::writeToPacket).decoder(SyncBagusAnimationsMessage::readFromPacket)
+                .consumerMainThread(SyncBagusAnimationsMessage::handle)
+                .add();
     }
 }
