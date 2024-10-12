5bccbff525c5e98fa675f1c27d6d259ae6c3edbf
baguchan <shutux500@gmail.com>
2024-10-13 08:29:26 +0900

    1.21.1-13.4.1 support json animation

--- src/main/java/bagu_chan/bagus_lib/api/client/IRootModel.java
+++ src/main/java/bagu_chan/bagus_lib/api/client/IRootModel.java
@@ -3,6 +3,7 @@ package bagu_chan.bagus_lib.api.client;
 import net.minecraft.client.animation.AnimationDefinition;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.world.entity.AnimationState;
+import net.neoforged.neoforge.client.entity.animation.json.AnimationHolder;
 import org.joml.Vector3f;
 
 import java.util.Optional;
@@ -43,6 +44,26 @@ public interface IRootModel {
 
     default void applyStaticBagu(AnimationDefinition animationDefinition) {
         KeyframeBagusAnimations.animate(this, animationDefinition, 0L, 1.0F, getCacheVec());
+    }
+
+
+    default void animateBagu(AnimationState animationState, AnimationHolder animationDefinition, float ageInTick) {
+        this.animateBagu(animationState, animationDefinition, ageInTick, 1.0F);
+    }
+
+    default void animateWalkBagu(AnimationHolder animationDefinition, float limbSwing, float limbSwingScale, float animationSpeedScale, float animationScale) {
+        long i = (long) (limbSwing * 50.0F * animationSpeedScale);
+        float f = Math.min(limbSwingScale * animationScale, 1.0F);
+        KeyframeBagusAnimations.animate(this, animationDefinition.get(), i, f, getCacheVec());
+    }
+
+    default void animateBagu(AnimationState animationState, AnimationHolder animationDefinition, float ageInTick, float animationSpeedScale) {
+        animationState.updateTime(ageInTick, animationSpeedScale);
+        animationState.ifStarted(p_233392_ -> KeyframeBagusAnimations.animate(this, animationDefinition.get(), p_233392_.getAccumulatedTime(), 1.0F, getCacheVec()));
+    }
+
+    default void applyStaticBagu(AnimationHolder animationDefinition) {
+        KeyframeBagusAnimations.animate(this, animationDefinition.get(), 0L, 1.0F, getCacheVec());
     }
 
 }
