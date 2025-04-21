bbbb026c25e1d899dcf3d78edf68568abdf49ded
baguchan <shutux500@gmail.com>
2025-04-21 18:09:45 +0900

    1.21.1-13.9.0

--- src/main/java/bagu_chan/bagus_lib/util/CollideUtil.java
+++ src/main/java/bagu_chan/bagus_lib/util/CollideUtil.java
@@ -23,7 +23,7 @@ public class CollideUtil {
         Vec3 motion = position.subtract(getPrevPositionVec(collideEntity));
         List<Entity> entitiesWithinAABB = world.getEntitiesOfClass(Entity.class, bounds.inflate(2));
         for (Entity entity : entitiesWithinAABB) {
-            if (entity != collideEntity) {
+            if (entity != collideEntity && !collideEntity.isPassenger()) {
                 Vec3 entityPosition = entity.position();
                 Vec3 entityMotion = entity.getDeltaMovement();
                 Vec3 vec3 = entity.collide(motion);
