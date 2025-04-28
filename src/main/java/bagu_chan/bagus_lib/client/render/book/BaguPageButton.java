2673947e0a3c47e5487517d03cbbc17b53284623
baguchan <shutux500@gmail.com>
2025-04-29 01:21:45 +0900

    1.21.1-13.11.0

---
+++ src/main/java/bagu_chan/bagus_lib/client/render/book/BaguPageButton.java
@@ -0,0 +1,35 @@
+package bagu_chan.bagus_lib.client.render.book;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.minecraft.client.sounds.SoundManager;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.sounds.SoundEvents;
+
+
+public class BaguPageButton extends Button {
+    private final Book book;
+    private final boolean isForward;
+    private final boolean playTurnSound;
+
+    public BaguPageButton(int x, int y, Book book, boolean isForward, boolean turnSound, OnPress onPress) {
+        super(x + (isForward ? -1 : 1) * book.buttonXOffset(), y - book.buttonYOffset(), book.buttonWidth(), book.buttonHeight(), CommonComponents.EMPTY, onPress, DEFAULT_NARRATION);
+        this.book = book;
+        this.isForward = isForward;
+        this.playTurnSound = turnSound;
+    }
+
+    @Override
+    public void renderWidget(GuiGraphics guiGraphics, int i, int j, float f) {
+        guiGraphics.blit(RenderType::guiTextured, isForward ? book.rightButton() : book.leftButton(), this.getX(), this.getY(), 0, 0, book.buttonWidth(), book.buttonHeight(), book.buttonWidth(), book.buttonHeight());
+    }
+
+    @Override
+    public void playDownSound(SoundManager soundManager) {
+        if (this.playTurnSound) {
+            soundManager.play(SimpleSoundInstance.forUI(SoundEvents.BOOK_PAGE_TURN, 1.0F));
+        }
+    }
+}
