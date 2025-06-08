02f2d4add8df052dc6e28079f2861345597343a2
baguchan <shutux500@gmail.com>
2025-06-08 21:44:35 +0900

    1.21.5-16.9.0 animation data

--- src/main/java/baguchi/bagus_lib/util/data/BagusAnimationData.java
+++ src/main/java/baguchi/bagus_lib/util/data/BagusAnimationData.java
@@ -1,6 +1,6 @@
 package baguchi.bagus_lib.util.data;
 
-import baguchi.bagus_lib.util.client.BagusAnimationUtil;
+import baguchi.bagus_lib.util.client.AnimationUtil;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 
@@ -30,7 +30,7 @@ public class BagusAnimationData {
 
     public void start(Entity entity) {
         if (!entity.level().isClientSide()) {
-            BagusAnimationUtil.sendAnimation(entity, this.animation);
+            AnimationUtil.sendAnimation(entity, this.animation);
 
             this.animationTick = 0;
             this.started = true;
@@ -39,7 +39,7 @@ public class BagusAnimationData {
 
     public void stop(Entity entity) {
         if (!entity.level().isClientSide()) {
-            BagusAnimationUtil.sendStopAnimation(entity, this.animation);
+            AnimationUtil.sendStopAnimation(entity, this.animation);
 
             this.animationTick = 0;
             this.started = false;
